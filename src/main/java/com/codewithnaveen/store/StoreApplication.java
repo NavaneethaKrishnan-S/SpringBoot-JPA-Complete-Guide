55a931230ffc11cba68d7eae0a3d2970ba133474
NavaneethaKrishnan-S <naveensaravana8@gmail.com>
2025-06-19 14:58:36 +0530

    feat(dev): Managing transactions, Transient - persistent - detached

--- src/main/java/com/codewithnaveen/store/StoreApplication.java
+++ src/main/java/com/codewithnaveen/store/StoreApplication.java
@@ -1,7 +1,6 @@
 package com.codewithnaveen.store;
 
-import com.codewithnaveen.store.entities.User;
-import com.codewithnaveen.store.repositories.UserRepository;
+import com.codewithnaveen.store.services.UserService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -11,14 +10,8 @@ public class StoreApplication {
 
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(StoreApplication.class);
-		var repository = context.getBean(UserRepository.class);
 
-		var user = User.builder()
-				.name("john")
-				.email("john@email.com")
-				.password("password")
-				.build();
-
-		repository.save(user);
+		var userService = context.getBean(UserService.class);
+		userService.showEntityState();
 	}
 }
