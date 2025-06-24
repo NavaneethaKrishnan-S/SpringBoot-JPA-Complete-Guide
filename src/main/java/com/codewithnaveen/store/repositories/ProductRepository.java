6977fea67c8ef8a1d0b96b637515b8d878951092
NavaneethaKrishnan-S <naveensaravana8@gmail.com>
2025-06-24 13:05:58 +0530

    feat(dev): Derived query methods

--- src/main/java/com/codewithnaveen/store/repositories/ProductRepository.java
+++ src/main/java/com/codewithnaveen/store/repositories/ProductRepository.java
@@ -3,5 +3,36 @@ package com.codewithnaveen.store.repositories;
 import com.codewithnaveen.store.entities.Product;
 import org.springframework.data.repository.CrudRepository;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 public interface ProductRepository extends CrudRepository<Product, Long> {
+
+    //String
+    List<Product> findByName(String name);
+    List<Product> findByNameLike(String name);
+    List<Product> findByNameNotLike(String name);
+    List<Product> findByNameStartingWith(String name);
+    List<Product> findByNameEndingWith(String name);
+    List<Product> findByNameEndingWithIgnoreCase(String name);
+
+    //Numbers
+    List<Product> findByPrice(BigDecimal price);
+    List<Product> findByPriceGreaterThan(BigDecimal price);
+    List<Product> findByPriceGreaterThanEqual(BigDecimal price);
+    List<Product> findByPriceLessThanEqual(BigDecimal price);
+    List<Product> findByPriceBetween(BigDecimal price);
+
+    //Null
+    List<Product> findByDescriptionNull();
+    List<Product> findByDescriptionNotNull();
+
+    //Multiple Conditions
+    List<Product> findByDescriptionNullAndNameNull();
+
+    //Sort(OrderBy)
+    List<Product> findByNameOrderByNameAsc(String name);
+
+    //limits (top/first)
+    List<Product> findTop5ByNameOrderByPrice(String name);
 }
