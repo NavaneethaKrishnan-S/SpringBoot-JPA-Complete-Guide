b3c73deae22c2b4821150c4cb1be81e4bae5a28b
NavaneethaKrishnan-S <naveensaravana8@gmail.com>
2025-06-30 11:41:22 +0530

    feat(dev): Avoiding the N+1 problem

--- src/main/java/com/codewithnaveen/store/services/UserService.java
+++ src/main/java/com/codewithnaveen/store/services/UserService.java
@@ -142,4 +142,13 @@ public class UserService {
         System.out.println(user);
     }
 
+    @Transactional
+    public void fetchUsers(){
+        var users = userRepository.findAllWithAddresses();
+        users.forEach(u -> {
+            System.out.println(u);
+            u.getAddresses().forEach(System.out::println);
+        });
+    }
+
 }
